1349361e5b5bee26754b8a26b60aaf3821d2e814
jenna-dietsch <30694833+jenna-dietsch@users.noreply.github.com>
2020-05-04 22:02:28 -0400

    Update transform_nyt_covid
    use pivot longer to match counties data table structure, create new covid table in locals db

--- code/transform/transform_nyt_covid.R
+++ code/transform/transform_nyt_covid.R
@@ -11,4 +11,17 @@ state_covid <- state_covid %>%
   mutate(date = as.Date(date, format = "%Y-%m-%d"))
 
 county_covid <- county_covid %>%
-  mutate(date = as.Date(date, format = "%Y-%m-%d"))
+  pivot_longer(
+    cols = c(cases, deaths),
+    names_to = "var_name"
+  ) %>%
+  mutate(
+    dataset = 'nyt_covid',
+    date = as.Date(date, format = "%Y-%m-%d"),
+    stat_type = 'n'
+  ) %>%
+  select(dataset, state, county, fips, date, var_name, value, stat_type)
+  
+odbc::dbWriteTable(locals_db, "covid", county_covid, append = F)
+
+rm(us_covid); rm(state_covid); rm(county_covid)
