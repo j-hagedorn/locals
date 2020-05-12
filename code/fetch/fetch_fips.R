dde7dee6eb77e2fb423efb365cc4e3b44ecbfea1
Joshh <j-hagedorn@users.noreply.github.com>
2020-05-12 14:01:34 -0400

    Update fetch_fips.R

--- code/fetch/fetch_fips.R
+++ code/fetch/fetch_fips.R
@@ -1,19 +1,75 @@
 ## fetch_fips.R
 
-library(httr); library(readxl)
+library(tidyverse); library(tidycensus);library(sf)
+options(tigris_use_cache = TRUE)
 
-GET("https://www2.census.gov/programs-surveys/popest/geographies/2018/state-geocodes-v2018.xlsx", 
-    write_disk(tf <- tempfile(fileext = ".xlsx")))
+# Download block group geometries
 
-state_fips <- read_excel(tf, skip = 4, col_names = TRUE) 
+fips_blockgroup <- tibble()
 
-
-GET("https://www2.census.gov/programs-surveys/popest/geographies/2018/all-geocodes-v2018.xlsx", 
-    write_disk(tf <- tempfile(fileext = ".xlsx")))
-
-all_fips <- read_excel(tf, skip = 3, col_names = TRUE) %>%
-  mutate(
-    county_state_fips = paste0(`State Code (FIPS)`, `County Code (FIPS)`),
-    county_state_fips = as.integer(county_state_fips)
-  )
+for (i in unique(fips_codes$state[!fips_codes$state %in% c('AS','GU','MP','PR','UM','VI')])) {
   
+  sf <-
+    get_acs(
+      geography = "block group", variables = "B00001_001",
+      cache_table = T, state = i, geometry = T, keep_geo_vars = T
+    )
+  fips_blockgroup <- bind_rows(fips_blockgroup,sf)
+  rm(sf);rm(i)
+  
+}  
+
+fips_blockgroup <- 
+  fips_blockgroup %>%
+  rename_all(list(~str_to_lower(.))) %>%
+  rename_at(vars(any_of(c('statefp','countyfp'))),list(~str_replace(.,"fp$","_id"))) %>%
+  rename_at(vars(any_of(c('tractce','blkgrpce'))),list(~str_replace(.,"ce$","_id"))) %>%
+  rename(pop = estimate, name = name.y) %>%
+  select(-affgeoid,-name.x,-variable,-lsad) %>%
+  st_as_sf(sf_column_name = "geometry") %>%
+  # Label block-specific vars with 'blkgrp_' prefix
+  rename_at(vars(!ends_with("_id")),list(~paste0(.,"_blkgrp"))) 
+
+# Save as .shp, since SQL can't hold nested lists  
+sf::st_write(fips_blockgroup,"data/fips_blockgroup.shp",append=F)
+
+ 
+  
+  
+  mapview::mapView(tst, zcol = "estimate", legend = TRUE) 
+  
+  tst %>%
+  ggplot(aes(fill = pop_blkgrp)) + 
+  geom_sf(color = NA) + 
+  #coord_sf(crs = 26911) + 
+  scale_fill_viridis_c(option = "magma") 
+
+# Download tract geometries
+
+fips_tract <- tibble()
+
+for (i in unique(fips_codes$state[!fips_codes$state %in% c('AS','GU','MP','PR','UM','VI')])) {
+  
+  sf <-
+    get_acs(
+      geography = "tract", variables = "B00001_001",
+      cache_table = T, state = i, geometry = T, keep_geo_vars = T
+    )
+  fips_tract <- bind_rows(fips_tract,sf)
+  rm(sf);rm(i)
+  
+}  
+
+tst <- 
+  fips_tract %>%
+  rename_all(list(~str_to_lower(.))) %>%
+  rename_at(vars(any_of(c('statefp','countyfp'))),list(~str_replace(.,"fp$","_id"))) %>%
+  rename_at(vars(any_of(c('tractce','blkgrpce'))),list(~str_replace(.,"ce$","_id"))) %>%
+  rename(pop = estimate, name = name.y) %>%
+  select(-affgeoid,-name.x,-variable,-lsad) %>%
+  st_as_sf(sf_column_name = "geometry") %>%
+  # Label tract-specific vars with '_tract' suffix
+  rename_at(vars(!ends_with("_id")),list(~paste0(.,"_tract"))) 
+
+# Save as .shp, since SQL can't hold nested lists  
+sf::st_write(fips_tract,"data/fips_tract.shp",append=F)
