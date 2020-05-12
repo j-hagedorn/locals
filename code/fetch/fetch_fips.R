6f85e0c6b9aa26453d281fcfca68154f875c70b8
Joshh <j-hagedorn@users.noreply.github.com>
2020-05-12 17:01:15 -0400

    Update fetch_fips.R

--- code/fetch/fetch_fips.R
+++ code/fetch/fetch_fips.R
@@ -97,6 +97,46 @@ fips_county <-
 
 save(fips_county,file = "data/fips_county.rda")
 
+# Download county geometries
+
+fips_state <- tibble()
+
+for (i in unique(fips_codes$state[!fips_codes$state %in% c('AS','GU','MP','PR','UM','VI')])) {
+  
+  sf <-
+    get_acs(
+      geography = "state", variables = "B00001_001",
+      cache_table = T, state = i, geometry = T, keep_geo_vars = T
+    )
+  fips_state <- bind_rows(fips_state,sf)
+  rm(sf);rm(i)
+  
+}  
+
+fips_state <-
+  fips_state %>%
+  rename_all(list(~str_to_lower(.))) %>%
+  rename_at(vars(any_of(c('statefp','countyfp'))),list(~str_replace(.,"fp$","_id"))) %>%
+  rename_at(vars(any_of(c('tractce','blkgrpce'))),list(~str_replace(.,"ce$","_id"))) %>%
+  rename(pop = estimate, name = name.y) %>%
+  select(-affgeoid,-statens,-name.x,-variable,-stusps,-lsad) %>%
+  # Convert area to square miles
+  mutate(sqmi = aland / 2589988) %>%
+  # Label tract-specific vars with '_tract' suffix
+  rename_at(vars(!ends_with("_id")),list(~paste0(.,"_state"))) 
+
+save(fips_state,file = "data/fips_state.rda")
+
+# Join tract and block groups
+
+fips <-
+  fips_state %>%
+  left_join(fips_county,by = c("state_id")) %>%
+  left_join(fips_tract,by = c("state_id","county_id")) %>%
+  left_join(fips_blockgroup,by = c("state_id","county_id","tract_id"))
+
+save(fips,file = "data/fips.rda")
+
 # Convert to shapefiles
 
 fips_county_shape <-
@@ -111,30 +151,5 @@ fips_block_shape <-
   fips_blockgroup %>%
   st_as_sf(sf_column_name = "geometry_blkgrp")
 
-# Save as .shp, since SQL can't hold nested lists  
-st_write(fips_block_shape,"data/fips_block_shape.shp",append = F)
-blk_shp_cols <- colnames(fips_block_shape)
-fips_block_shape <- sf::read_sf("data/fips_block_shape.shp")
-colnames(fips_block_shape) <- blk_shp_cols # Replace colnames b/c ESRI files truncate
-
-# Save as .shp, since SQL can't hold nested lists  
-st_write(fips_tract_shape,"data/fips_tract_shape.shp",append = F)
-trct_shp_cols <- colnames(fips_tract_shape)
-tst <- sf::read_sf("data/fips_tract_shape.shp")
-colnames(fips_tract_shape) <- trct_shp_cols # Replace colnames b/c ESRI files truncate
 
 
-  
-
-
-# Save as .shp, since SQL can't hold nested lists  
-sf::st_write(fips_county_shape,"data/fips_county_shape.shp",append=F)
-fips_county_shape <- sf::read_sf("data/fips_county_shape.shp")
-
----
-
-# Join tract and block groups
-
-tst <-
-  fips_county %>%
-  left_join(fips_blockgroup,by = c("state_id","county_id","tract_id"))
