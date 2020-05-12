ad272cf0ed14c52031d0cb97c303b1024333d9e4
Joshh <j-hagedorn@users.noreply.github.com>
2020-05-12 17:00:58 -0400

    Update read_odbc.R

--- code/connect/read_odbc.R
+++ code/connect/read_odbc.R
@@ -2,9 +2,13 @@
 
 library(DBI); library(odbc);library(dbplyr);library(tidyverse);library(lubridate)
 
+# Geographic reference data stored as .shp
+
+
+
 locals_db <- DBI::dbConnect(odbc::odbc(), "locals")
 
-# Create local references to SWMBH tables ####
+# Create local references to tables ####
 
 tracts_db <- tbl(locals_db, "tracts")
 counties_db <- tbl(locals_db, "counties")
@@ -13,3 +17,4 @@ covid_db <- tbl(locals_db, "covid")
 tracts_db %>%
   group_by(dataset) %>%
   summarize(n = n())
+
