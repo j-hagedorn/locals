55fe1d390dec3e1d92b5b1869b5837b5e93f51db
JoeT531 <57190859+JoeT531@users.noreply.github.com>
2020-05-14 10:13:26 -0400

    adding address level datasets to locals databse

---
+++ code/fetch/fetch_addresses.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+hospitals<-read_csv("https://opendata.arcgis.com/datasets/6ac5e325468c4cb9b905f1728d6fbf0f_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D")
+
+law_enf<-read_csv("https://opendata.arcgis.com/datasets/0d79b978d71b4654bddb6ca0f4b7f830_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D")
+
+nursing<-read_csv("https://opendata.arcgis.com/datasets/78c58035fb3942ba82af991bb4476f13_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D")
+
+prison<-read_csv('https://opendata.arcgis.com/datasets/2d6109d4127d458eaf0958e4c5296b67_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D')
+
+
