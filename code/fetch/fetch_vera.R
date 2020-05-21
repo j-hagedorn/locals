5f9022e66a7f7adcf7e7b79c46986660853f7496
JoeT531 <57190859+JoeT531@users.noreply.github.com>
2020-05-21 18:02:10 -0400

    updated documentation and appended to locals counties

--- code/fetch/fetch_vera.R
+++ code/fetch/fetch_vera.R
@@ -3,8 +3,6 @@
 vera<-read_csv("https://github.com/vera-institute/incarceration_trends/blob/master/incarceration_trends.csv?raw=true")
 
 
-
-
     
 
 
--- code/transform/transform_vera.R
+++ code/transform/transform_vera.R
@@ -1,23 +1,53 @@
 
 
 vera_df<-vera%>%
-  filter(year > 1983)%>%
-  select(date = year,county_id = fips,name = county_name,
-         total_pop,total_prison_pop,total_jail_pop,total_jail_pretrial,total_jail_from_prison,
-         black_prison_pop,white_prison_pop,latinx_prison_pop,aapi_prison_pop, male_prison_pop,female_prison_pop,
-         black_jail_pop, white_jail_pop,latinx_jail_pop,aapi_jail_pop,male_jail_pop,female_jail_pop
-  )%>%
-  mutate(dataset = 'vera',
-         id = NA_character_)%>%
-  pivot_longer(cols = c(total_pop,total_prison_pop,total_jail_pop,total_jail_pretrial,total_jail_from_prison,
-                        black_prison_pop,white_prison_pop,latinx_prison_pop,aapi_prison_pop, male_prison_pop,female_prison_pop,
-                        black_jail_pop, white_jail_pop,latinx_jail_pop,aapi_jail_pop,male_jail_pop,female_jail_pop),
-               names_to = "var_name",values_to = "value")%>%
-  select(dataset,id,name,date,county_id,var_name,value)
+      filter(year > 1983)%>%
+      select( year,county = fips,name = county_name,
+              total_prison_pop,total_jail_pop,total_jail_pretrial,total_jail_from_prison,
+              black_prison_pop,white_prison_pop,latinx_prison_pop,aapi_prison_pop, male_prison_pop,female_prison_pop,
+              black_jail_pop, white_jail_pop,latinx_jail_pop,aapi_jail_pop,male_jail_pop,female_jail_pop,
+              native_prison_pop,native_jail_pop,other_race_prison_pop,other_race_jail_pop
+      )%>%
+      mutate( dataset = 'vera',
+              state = substr(county,1,2),
+              stat_type = 'n'
+      )%>%
+      
+      pivot_longer(cols = c(total_prison_pop,total_jail_pop,total_jail_pretrial,total_jail_from_prison,
+                            
+                            
+                    black_prison_pop,white_prison_pop,latinx_prison_pop,aapi_prison_pop,native_prison_pop,
+                    other_race_prison_pop,
+                    male_prison_pop,female_prison_pop,
+                    
+                    black_jail_pop, white_jail_pop,latinx_jail_pop,aapi_jail_pop,native_jail_pop,
+                    other_race_jail_pop,
+                    male_jail_pop,female_jail_pop
+                    
+                    ),
+                    names_to = "var_name",values_to = "value"
+      )%>%
+      mutate(race = case_when(str_detect(var_name,"black")~"black", 
+                              str_detect(var_name,"white")~"white",
+                              str_detect(var_name,"latinx")~"hispanic",
+                              str_detect(var_name,"aapi")~"pacific",
+                              str_detect(var_name,"native")~"natam",
+                              str_detect(var_name,"other")~"other",
+                              
+                              TRUE ~ "pooled"),
+            gender = case_when(str_detect(var_name,"female")~"female", 
+                              str_detect(var_name,"male")~"male",
+                              TRUE ~ "pooled"),
+            age_range = "pooled",
+            year = as.character(year),
+            value = as.character(value)
+      )%>%
+      select(dataset,state,county,year,race,gender,age_range,var_name,value,stat_type)
+
 
 
 #===========================
-# Append to WSU Crime Data
+# Append to counties
 #===========================
 
 library(DBI)
@@ -25,11 +55,8 @@ library(DBI)
 # database connection 
 locals_db <- DBI::dbConnect(odbc::odbc(), "locals")
 
-dbWriteTable(conn =  locals_db,
-              "wsu_public_county",
-               vera_df,
-               append = TRUE
 
-              )
+dbWriteTable(locals_db, name="counties", value= vera_df , append=T, row.names=F, overwrite=F)
+
 
 
