f5dd52ce024ebd690cad026d3b13661c6953c0bb
JoeT531 <57190859+JoeT531@users.noreply.github.com>
2020-05-21 14:59:31 -0400

    Documentation added and data appended to counties table in locals

--- code/transform/transform_hpsa.R
+++ code/transform/transform_hpsa.R
@@ -6,16 +6,23 @@ pc<-hpsa_primary_care%>%
            id = `HPSA ID`, 
            designation_type = `Designation Type`,
            hpsa_status = `HPSA Status`,
-           date = `HPSA Designation Last Update Date`,
-           county_id = `Common State County FIPS Code`
+           year = `HPSA Designation Last Update Date`,
+           county = `Common State County FIPS Code`,
+           state = `Common State FIPS Code`
           
           )%>%
-          mutate(dataset = 'hpsa_primary_care'
+          mutate(dataset = 'hpsa_primary_care',
+                 race = NA_character_,
+                 gender = NA_character_,
+                 age_range = NA_character_,
+                 stat_type = 'cat'
+                 
+                 
           )%>%
           pivot_longer(cols = c(designation_type,hpsa_status), names_to = "var_name",
                                  values_to = "value"
           )%>%
-          select(dataset,id,name,date,county_id,var_name,value)
+          select(dataset,state,county,year,race,gender,age_range,var_name,value,stat_type)
 
 # Mental health
 mh<-hpsa_mental_health%>%
@@ -23,35 +30,34 @@ mh<-hpsa_mental_health%>%
          id = `HPSA ID`, 
          designation_type = `Designation Type`,
          hpsa_status = `HPSA Status`,
-         date = `HPSA Designation Last Update Date`,
-         county_id = `Common State County FIPS Code`
+         year = `HPSA Designation Last Update Date`,
+         county = `Common State County FIPS Code`,
+         state = `Common State FIPS Code`
          
-      )%>%
-      mutate(dataset = 'hpsa_mental_health')%>%
-      pivot_longer(cols = c(designation_type,hpsa_status), names_to = "var_name",
-                             values_to = "value")%>%
-      select(dataset,id,name,date,county_id,var_name,value)
+  )%>%
+  mutate(dataset = 'hpsa_mental_health',
+         race = NA_character_,
+         gender = NA_character_,
+         age_range = NA_character_,
+         stat_type = 'cat'
+         
+         
+  )%>%
+  pivot_longer(cols = c(designation_type,hpsa_status), names_to = "var_name",
+               values_to = "value"
+  )%>%
+  select(dataset,state,county,year,race,gender,age_range,var_name,value,stat_type)
   
 
 # Combine 
-
 full<-rbind(pc,mh)
 
-#=======================
-#Puch back to database
-#=======================
-
-# database connection 
-locals_db <- DBI::dbConnect(odbc::odbc(), "locals")
+#===========================
+# Append to master counties
+#===========================
 
 
-copy_to(
-  dest = locals_db,
-  df = full,
-  name = "wsu_public_county",
-  overwrite = T,
-  temporary = F
-)
+dbWriteTable(locals_db, name="counties", value=full , append=T, row.names=F, overwrite=F)
 
 
 
