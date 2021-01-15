8c2d8760b84605cf378232d5d9a89817cf0b0a16
JoeT531 <57190859+JoeT531@users.noreply.github.com>
2021-01-15 08:07:46 -0500

    updating census county

--- code/fetch_acs_county_test.R
+++ code/fetch_acs_county_test.R
@@ -13,45 +13,45 @@ census_dic<-read_csv("data/census_var_dic.csv")
 
 
 acs_vars_by_year<-c(
-  'B01003_001', # total population 
-  'B06012_002', # total below poverty 
-  'B02001_003', # total African-American 
-  'B02001_002', # total White 
-  'B03001_003', # total Latino
-  'B02001_005', # total Asian
-  'B11012_015', # total single dad households 
-  'B11012_010', # total single mom households 
-  'B25071_001', # rent as a percent of income 
-  'B25012_010', # total renter occupied homes 
-  'B25012_002', # total owner occupied homes 
-  'B11001_003', # total married couples family 
-  'B25010_001', # average household size 
-  'B10002_002', # total grandparent households responsible for own grandchildren
-  'B06009_003', # total high school graduates (including equivalency)
-  'B06009_004', # total some college or associates degree
-  'B06009_005', # total bachelor's degree 
-  'B06009_006', # total graduate or professional degree 
-  'B06009_007', # total born in state of residence
-  'B21001_002', # total veteran 
-  'B07001_066', # total moved from different state in the last 1-4 years 
-  'B07001_065', # total moved from different state 
-  'B05002_013', # total foreign born 
-  'B23025_007', # total not in the labor force 
-  'B23025_002', # total labor force 
-  'B23025_005', # total employed 
-  'B23007_007', # total two parent working households with children 
-  'B23007_009', # total two parent households with stay-at-home mom
-  'B19057_002', # total receiving public assistance income last 12 months 
-  'B08301_010', # total taking public transportation to work
-  'B19053_002', # total receiving self-employment income
-  'B08128_003', # total private company workers 
-  'B08128_005', # total non-profit workers 
-  'B08128_004', # total self employed worker 
-  'B08128_006', # total local government worker 
-  'B08128_007', # total state government worker 
-  'B08128_008', # total federal government worker 
-  'B08128_009', # total self-employed contractors 
-  'B08128_010', # total unpaid-family workers (children or family helping business)
+  'B01003_001', #  total population 
+  'B06012_002', #  total below poverty 
+  'B02001_003', #  total African-American 
+  'B02001_002', #  total White 
+  'B03001_003', #  total Latino
+  'B02001_005', #  total Asian
+  'B11012_015', #  total single dad households 
+  'B11012_010', #  total single mom households 
+  'B25071_001', #  rent as a percent of income 
+  'B25012_010', #  total renter occupied homes 
+  'B25012_002', #  total owner occupied homes 
+  'B11001_003', #  total married couples family 
+  'B25010_001', #  average household size 
+  'B10002_002', #  total grandparent households responsible for own grandchildren
+  'B06009_003', #  total high school graduates (including equivalency)
+  'B06009_004', #  total some college or associates degree
+  'B06009_005', #  total bachelor's degree 
+  'B06009_006', #  total graduate or professional degree 
+  'B06009_007', #  total born in state of residence
+  'B21001_002', #  total veteran 
+  'B07001_066', #  total moved from different state in the last 1-4 years 
+  'B07001_065', #  total moved from different state 
+  'B05002_013', #  total foreign born 
+  'B23025_007', #  total not in the labor force 
+  'B23025_002', #  total labor force 
+  'B23025_005', #  total employed 
+  'B23007_007', #  total two parent working households with children 
+  'B23007_009', #  total two parent households with stay-at-home mom
+  'B19057_002', #  total receiving public assistance income last 12 months 
+  'B08301_010', #  total taking public transportation to work
+  'B19053_002', #  total receiving self-employment income
+  'B08128_003', #  total private company workers 
+  'B08128_005', #  total non-profit workers 
+  'B08128_004', #  total self employed worker 
+  'B08128_006', #  total local government worker 
+  'B08128_007', #  total state government worker 
+  'B08128_008', #  total federal government worker 
+  'B08128_009', #  total self-employed contractors 
+  'B08128_010', #  total unpaid-family workers (children or family helping business)
   'B11001_001', # total households including living alone 
   'C27006_004', # total medicare coverage for males under 19
   'C27006_007', # total medicare coverage for males 19 to 64
@@ -72,8 +72,7 @@ acs_vars_by_year<-c(
   'B25024_010', # total mobile home units 
   'B25024_007', # total housing with units between 10 and 19
   'B25024_008', # total housing with units 20 and over 
-  'B19013_001', # median household income (inflation adjusted)
-  'B25004_008'  # Other vacant housing units (includes vacant housing such as abandoned,needs repair and others see : https://www.census.gov/housing/hvs/definitions.pdf#:~:text=A%20housing%20unit%20is%20vacant%20if%20no%20one,a%20usual%20residence%20elsewhere.%20New%20units%20not%20yet)
+  
   
 )
 
@@ -162,7 +161,7 @@ df<-acs5_county %>%
   
 df%>%
   filter(county == '26163')%>%
-  filter(var_name == 'self_employed_worker_labor')%>%
+  filter(var_name == 'no_health_insurance_19to34_pop')%>%
   select(year,value)%>%
   ggplot(aes(x = year, y = value)) + geom_point()+geom_line()
   
