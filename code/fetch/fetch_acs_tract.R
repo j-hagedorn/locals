8b0803bce533223c94e558ba816afc844bcd7342
joet <joet@tbdsolutions.com>
2022-09-22 07:58:21 -0400

    updating tract data

--- code/fetch/fetch_acs_tract.R
+++ code/fetch/fetch_acs_tract.R
@@ -1,267 +1,271 @@
 library(tidycensus)
 library(tidyverse)
 library(lubridate)
-library(DBI)
-
-#======================#
-# Introduction ====
-#======================#
-
-# The script is intended to pull variables from the census api and store in our database. 
-# Any variable needs to be explicitly named and documented in the data_dictionary CSV
-# as well as in the code below. In general, the naming convention takes the following form: 
-
-# variable_by_group_universe
-
-# Where:
-# variable is the name of the measure (medicaid)
-# by group is any subset of the variable (medicaid_under65)
-# universe is the grain at which the variable is taken (medicaid_under65_pop)
-
-# For example, the variable B11012 looks at female householders with no spouse or parent 
-# but with their own children 18 and under; basically single mother households. 
-
-# The variable would be single householders,the by-group is mothers and the universe the number of homes.
-# Therefore the variable name is named: 
-
-# single_mom_homes
+library(sf)
 
 
+
+standard_name_format = function(x){
+  
+  x<- str_squish(x)
+  x<- str_replace_all(x," ","_")
+  x<- str_to_lower(x)
+  x<- str_replace_all(x,"_na","")
+  x
+}
+
+#================================#
+# Pulling in variable names ====
+#================================#
 
 # Use this query to scan different variable: 
 
-census_api_key(Sys.getenv("CENSUS_API_KEY"))
+census_api_key(Sys.getenv("CENSUS_API_KEY"),install = TRUE)
+
+v <- load_variables(2020, "acs5", cache = TRUE) %>% 
+  separate(label, into = paste0("label", 1:9), sep = "!!", fill = "right", remove = FALSE)
+
+v_profile <- load_variables(2020, "acs5/profile", cache = TRUE) %>% 
+  separate(label, into = paste0("label", 1:9), sep = "!!", fill = "right", remove = FALSE)
+
+v_subject<- load_variables(2020, "acs5/subject", cache = TRUE) %>% 
+  separate(label, into = paste0("label", 1:9), sep = "!!", fill = "right", remove = FALSE)
+
+v_frac<-
+  v_profile %>% 
+  filter(label1=='Percent') 
 
 
-v <- load_variables(2019, "acs5", cache = TRUE)
 
-census<-v%>%
-  # filter(str_detect(concept,'median')==T)%>%
-  #filter(str_detect(concept,'health')==T)%>%
-  filter(str_detect(label,'Income')==T)%>%
-  filter(str_detect(label,"Median")==T)
+profile_acs_variables <-
+  c(
+    'DP05_0001' # Total population 
+    # Education 
+    ,'DP02_0061P' # % 9-12th grade, no diploma
+    ,'DP02_0062P' # % 25 or over with HS diploma
+    ,'DP02PR_0063P' # % some college, no degree
+    ,'DP02PR_0068P' # % 25 or over with Bachelor's or higher
+    
+    # Employment 
+    ,'DP03_0009P' # % Unemployment Rate
+    ,'DP03_0002P' # % in the labor force
+    
+    # Household char
+    ,'DP02_0003P' # % married household with kids under 18
+    ,'DP03_0126P' # % Single mother families 
+    
+    #Housing 
+    ,'DP04_0142P' # % of households spending 35% or more of thier income on rent
+    ,"DP04_0047P" # % Renter occupied homes
+    ,'DP04_0046P' # % Owner occupied homes 
+    ,"DP04_0014P" # % Mobile Homes 
+    ,'DP04_0058P' # % Occupied housing with no vehicle
+    ,'DP04_0003P' # % vacant housing 
+    ,'DP04_0005P' # % Rental vacancey rate 
+    
+    # Health Care 
+    ,'DP02_0074P' # % Children Disabled under 18
+    ,'DP03_0099P' # % no health insurance, non-inst.,18-64
+    ,'DP03_0098P' # % public coverage 
+    ,'DP02_0072P' # % Disabled total
+    # Social Demographics 
+    ,'DP02_0070P' # % Veteran
+    ,'DP02_0094P' # % Foreign Born 
+    ,'DP05_0037P' # % White 
+    ,'DP05_0038P' # % Black
+    ,'DP05_0044P' # % Asian
+    ,'DP05_0039P' # % Ntv. American or Alaskan native
+    ,'DP05_0071P' # % Hispanic or Latino 
+    ,'DP05_0035P' # % Two or more races 
+    ,'DP05_0052P' # % Pacific Islander
+    ,'DP02_0036P' # % female Divorce Rate
+    # Economic 
+    ,'DP03_0072P' # % of households with public assistance income
+    ,'DP03_0062'  # Median Household Income 
+    
+  )
 
+industry_type<-
+  v_profile %>% 
+  filter(label1=='Percent',
+         label2 %in% c('INDUSTRY','OCCUPATION'), 
+         !name %in% c('DP03_0032P','DP03_0026P')) %>% 
+  select(name) %>% 
+  distinct() %>% pull()
 
-#======================================#
-# Variable List & Data Dictionary  ==== 
-#======================================#
-
-census_dic<-read_csv('docs/data_dictionary.csv')  %>%
-  filter(dataset =='acs_5',
-         !is.na(var_num))
-
-
-acs_vars_by_year<-c(
-  'B01003_001', # total population 
-  'B11001_001', # total homes 
-  'B23025_002', # total labor force 
-  'B06012_002', # total below poverty 
-  'B02001_003', # total African-American 
-  'B02001_002', # total White 
-  'B03001_003', # total Latino
-  'B02001_005', # total Asian
-  'B11012_015', # total single dad households 
-  'B11012_010', # total single mom households 
-  'B25071_001', # rent as a percent of income 
-  'B25012_010', # total renter occupied homes 
-  'B25012_002', # total owner occupied homes 
-  'B11001_003', # total married couples family 
-  'B25010_001', # average household size 
-  'B10002_002', # total grandparent households responsible for own grandchildren
-  'B06009_003', # total high school graduates (including equivalency)
-  'B06009_004', # total some college or associates degree
-  'B06009_005', # total bachelor's degree 
-  'B06009_006', # total graduate or professional degree 
-  'B06009_007', # total born in state of residence
-  'B21001_002', # total veteran 
-  'B07001_066', # total moved from different state in the last 1-4 years 
-  'B07001_065', # total moved from different state 
-  'B05002_013', # total foreign born 
-  'B23025_007', # total not in the labor force 
-  'B23025_005', # total employed 
-  'B23007_007', # total two parent working households with children 
-  'B23007_009', # total two parent households with stay-at-home mom
-  'B19057_002', # total receiving public assistance income last 12 months 
-  'B08301_010', # total taking public transportation to work
-  'B19053_002', # total receiving self-employment income
-  'B08128_003', # total private company workers 
-  'B08128_005', # total non-profit workers 
-  'B08128_006', # total local government worker 
-  'B08128_007', # total state government worker 
-  'B08128_008', # total federal government worker 
-  'B08128_009', # total self-employed contractors 
-  'B08128_010', # total unpaid-family workers (children or family helping business)
-  'C27006_004', # total medicare coverage for males under 19
-  'C27006_007', # total medicare coverage for males 19 to 64
-  'C27006_010', # total medicare coverage for males 65 and over 
-  'C27006_014', # total medicare coverage for females under 19
-  'C27006_017', # total medicare coverage for females 19 to 64
-  'C27006_020', # total medicare coverage for females 65 and over
-  'C27007_004', # total medicaid coverage for males under 19
-  'C27007_007', # total medicaid coverage for males 19 to 64
-  'C27007_010', # total medicaid coverage for males 65 and over 
-  'C27007_014', # total medicaid coverage for females under 19
-  'C27007_017', # total medicaid coverage for females 19 to 64
-  'C27007_020', # total medicaid coverage for females 65 and over 
-  'B27010_017', # total no health insurance under 19
-  'B27010_033', # total no health insurance 19 to 34
-  'B27010_050', # total no health insurance 35 to 64
-  'B27010_066', # total no health insurance 65 and over 
-  'B25024_010', # total mobile home units 
-  'B25024_007', # total housing with units between 10 and 19
-  'B25024_008', # total housing with units 20 and over 
-  
-  'B18101_004', #Total males with a disability under 5
-  'B18101_007', #Total males with a disability between 5 and 17
-  'B18101_010', #Total males with a disability between 18 and 34
-  'B18101_013', #Total males with a disability between 35 and 64
-  'B18101_016', #Total males with a disability between 65 and 74
-  'B18101_019', #Total males with a disability 75 and over
-  
-  'B18101_023', #Total females with a disability under 5
-  'B18101_026', #Total females with a disability between 5 and 17
-  'B18101_029', #Total females with a disability between 18 and 34
-  'B18101_032', #Total females with a disability between 35 and 64
-  'B18101_035', #Total females with a disability between 65 and 74
-  'B18101_038', #Total females with a disability 75 and over
-  
-  'B19013_001', #Median household income
-  'B25004_008', #Other vacant housing units (includes foreclosures/abandoned)
-  'B08128_004'  #Total self employed worker
-  
-)
-
-#==========================#
-# Census Table Pull ====
-#==========================#
+profile_acs_variables = c(profile_acs_variables, industry_type) %>% unique() 
 
 
-# Looping over the census api for each variable stated above for each year and 
-# for every state and storing in the acs5_tract tibble 
+
+#================================#
+# ACS Data Counties Profile ====
+#================================#
 
 
-acs5_tract <- tibble()
+# Profile ACS Variables 
 
-# use when getting data longitudinal 
-year_range<-2010:year(today())
-
+profile_acs<-tibble()
 
 for (st in unique(fips_codes$state[!fips_codes$state %in% c('AS','GU','MP','PR','UM','VI')])) {
   
-  for (v in acs_vars_by_year){ 
-    
-    # for( y in year_range){ 
-    
-    tryCatch(
-      
-      expr = {
-        df <- 
-          get_acs(
-            geography = "tract", 
-            variables = v, # Variable list
-            state = st , # State list
-            year = 2019 
-          ) %>%
-          mutate( year = "2019")
-        
-        
-        acs5_tract <- bind_rows(acs5_tract,df)
-        
-      }, 
-      error =  function(e){})
-    
-    #  } Year loop
-  }
+  
+  iter_x<-
+    get_acs(geography = 'tract'
+            ,state = st
+            ,variables = profile_acs_variables  
+            ,year = 2020
+            ,show_call = F) %>% 
+    left_join(v_profile, by = c('variable' = 'name')) %>% 
+    mutate_all(as.character)
+  
+  
+  profile_acs<-bind_rows(profile_acs, iter_x)
+  
 }
 
 
-# manipulating the results and adding the variable names from the data dictionary. 
-# The data dictionary should be updated with any new variables before joining. 
-
-df<-acs5_tract %>%
-  left_join(census_dic, by = c("variable" = 'var_num')) %>%
+profile_acs<-
+  profile_acs %>% 
   mutate(
-    dataset = 'acs_5',
-    state = substr(GEOID, 1,2),
-    county = substr(GEOID, 1,5), 
+    label5 = coalesce(label5,""),
+    var_name = str_to_lower(paste(label1,label3,label4,label5,sep = " ")),
     tract = GEOID,
-    value = estimate,
-    race = case_when(
-      str_detect(var_name,'latino') ~ "hispanic",
-      str_detect(var_name,'african') ~ "black",
-      str_detect(var_name,'white')~ "white",
-      str_detect(var_name,'asian')~ "asian",
-      TRUE ~ "pooled"
-    ),
-    gender = case_when(
-      str_detect(var_name,"female") ~ "female",
-      str_detect(var_name,"male") ~ "male",
-      TRUE ~ "pooled"
-    ),
-    age_range = case_when(
-      str_detect(var_name,'75over') ~ '75over',
-      str_detect(var_name,'65to74') ~ '65to74',
-      str_detect(var_name,'65over') ~ '65over',
-      str_detect(var_name,'35to64') ~ '35to64',
-      str_detect(var_name,'19to64') ~ '19to64',
-      str_detect(var_name,'18to34') ~ '18to34',
-      str_detect(var_name,'19to34') ~ '19to34',
-      str_detect(var_name,'5to17') ~ '5to17',
-      str_detect(var_name,'under19') ~'under19',
-      str_detect(var_name,'under5') ~ 'under5',
-      TRUE ~ "pooled"
-    ),
-    stat_type = case_when(
-      str_detect(var_name,"pop|labor|homes") ~ 'n',
-      TRUE ~ 'frac')
-  ) %>%
-  select(
-    dataset,state,county,tract,year,race,gender,age_range,var_name,value,stat_type
-  ) %>%
+    value = estimate) %>% 
+  select(variable,tract,var_name,value) %>% 
+  mutate_all(as.character) %>% 
+  # Remove empty variables 
+  mutate(var_check = ifelse(is.na(value),0,1)) %>% 
+  group_by(var_name,variable) %>% 
+  mutate(sum_check = sum(var_check,na.rm = T)) %>% 
+  ungroup() %>% 
+  filter(sum_check >0)  %>% 
+  select(-var_check,-sum_check) %>% 
   distinct()
 
 
+# Subject ACS Variables 
 
-#test<-df %>%
-#  filter(age_range == 'pooled') %>%
-#  select(var_name) %>%
-#  distinct()
+subject_acs<-tibble()
 
-
-
-#==========================#
-# Insert into database ====
-#==========================#
-
-# Connect to DB
-locals_db <- DBI::dbConnect(odbc::odbc(), "locals")
-
-
-# create sql query to delete the old data and insert the new
-vars<-
-  read_csv('docs/data_dictionary.csv')  %>%
-  filter(dataset =='acs_5') %>%
-  select(var_name)%>%
-  distinct()%>%
-  pull()
-
-vars_sql<- noquote(paste("'",as.character(vars),"'",collapse=", ",sep=""))
+for (st in unique(fips_codes$state[!fips_codes$state %in% c('AS','GU','MP','PR','UM','VI')])) {
+  
+  
+  iter_x<-
+    get_acs( geography = 'tract'
+             ,state = 'MI'
+             ,variables = c('S2704_C03_002','S2704_C03_006','S1701_C03_006')  
+             ,year = 2020
+             ,show_call = F) %>% 
+    left_join(v_subject, by = c('variable' = 'name')) %>% 
+    mutate_all(as.character)
+  
+  
+  subject_acs<-bind_rows( subject_acs, iter_x)
+  
+}
 
 
-delete_query<-
-  {paste("
-        delete
-        FROM [dbo].[tracts]
-        where var_name in (",vars_sql,") ",sep = "")
-    
-  }
+
+subject_acs<-
+  get_acs( geography = 'tract'
+           ,state = 'MI'
+           ,variables = c('S2704_C03_002','S2704_C03_006','S1701_C03_006')  
+           ,year = 2020
+           ,show_call = F) %>% 
+  left_join(v_subject, by = c('variable' = 'name')) %>% 
+  mutate_all(as.character)
+
+subject_acs<-
+  subject_acs %>% 
+  mutate(
+    label5 = coalesce(label5,""),
+    var_name = str_to_lower(paste(label1,label3,label4,label5,sep = " ")),
+    tract = GEOID,
+    value = estimate) %>% 
+  select(variable,tract,var_name,value) %>% 
+  mutate_all(as.character) %>% 
+  # Remove empty variables 
+  mutate(var_check = ifelse(is.na(value),0,1)) %>% 
+  group_by(var_name,variable) %>% 
+  mutate(sum_check = sum(var_check,na.rm = T)) %>% 
+  ungroup() %>% 
+  filter(sum_check >0)  %>% 
+  select(-var_check,-sum_check) %>% 
+  distinct()
 
 
-dbSendQuery(locals_db,delete_query)
+acs_data<-bind_rows(profile_acs,subject_acs)
+
+# Getting land area 
+
+tig = tibble()
+
+for (st in unique(fips_codes$state[!fips_codes$state %in% c('AS','GU','MP','PR','UM','VI')])) {
+  
+
+tig_iter<-
+  tigris::tracts(st) %>% 
+  st_set_geometry(NULL) %>% 
+  mutate(tract = GEOID) %>% 
+  select(tract,area = ALAND)
+
+tig = bind_rows(tig,tig_iter)
+
+}
+
+pop_density<-
+  acs_data %>% 
+  filter(variable == 'DP05_0001') %>% 
+  select(-variable) %>% 
+  mutate(value = case_when(is.na(value) ~ NA_real_, 
+                           T ~ as.numeric(value))) %>% 
+  na.omit() %>% 
+  pivot_wider(id_cols = tract,names_from = var_name,values_from = value) %>% 
+  rename_all(standard_name_format) %>% 
+  left_join(tig, by = c("tract")) %>% 
+  mutate(value = round((estimate_total_population/area)*1000,4),
+         variable = 'Derived',
+         var_name = 'Est. Population/Land Area') %>% 
+  select(variable,tract,var_name,value) %>% 
+  mutate_all(as.character)
+
+acs_data = bind_rows(acs_data,pop_density)
+
+rm(profile_acs,v_profile,tig,profile_acs_variables,analysis_df,
+   industry_type,profile_acs_variables,standard_name_format,v
+   ,v_frac,v_profile,v_subject,test,tig,pop_density,subject_acs)
 
 
-start<-Sys.time()
-# Writing updated or new variables to data base 
-odbc::dbWriteTable(locals_db, 'tracts', df, append = T)
+# Attach variable short names (Update with any new variables)
 
-end<-Sys.time()
+short_names<-
+  read_csv('data/census_dic.csv')
+
+
+df<-
+  acs_data %>% 
+  left_join(short_names,by = 'variable') %>% 
+  mutate(var_name = str_to_title(var_name),
+         year = '2020',
+         source = 'census') %>% 
+  select(source,year,tract,var_short_name,value,var_name,variable)
+
+
+
+
+rm(profile_acs,v_profile,tig,profile_acs_variables,analysis_df,
+   industry_type,profile_acs_variables,standard_name_format,v
+   ,v_frac,v_profile,v_subject,test,tig,pop_density,subject_acs)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- code/transform/transform_evict_tract.R
+++ code/transform/transform_evict_tract.R
@@ -1,50 +1,43 @@
+library(tidyverse)
+library(lubridate)
+library(DBI)
 
-evict_tract <- fetch_evictions_tract()
+evictions_source<-source("code/fetch/fetch_evictions_tract.R") 
+  
+evictions = 
+  evictions_source$value %>% 
+  filter(state == 'Michigan') %>% 
+  group_by(fips) %>% 
+  filter(year == max(year)) %>% 
+  ungroup() %>% 
+  mutate(judgement_rate = coalesce(judgement_rate,0),
+         source = 'eviction_labs',
+         tract = fips,
+         var_short_name = 'Eviction Rate',
+         value = coalesce(judgement_rate,0),
+         var_name = "Ratio of Evictions to Rental Housing Units",
+         variable = 'judgement_rate') %>% 
+  select(source,year,tract,var_short_name,value,var_name,variable) %>% 
+  mutate_all(as.character)
 
-df <-
-  evict_tract %>%
-  rename_all(list(~str_to_lower(.))) %>%
-  rename_all(list(~str_replace_all(.,"-","_"))) %>%
-  # Remove race, which is not a breakdown of evictions
-  select(-pct_white:-pct_other) %>%
-  select(geoid,year,renter_occupied_households:subbed) %>% 
-  mutate(
-    state = str_sub(geoid,1,2),
-    county = str_sub(geoid,3,5),
-    tract = str_sub(geoid,6,11)
-  ) %>%
-  select(
-    -geoid,-median_gross_rent, -median_household_income,
-    -median_property_value
-  ) %>%
-  pivot_longer(cols = -one_of("state","county","tract","year")) %>%
-  mutate(
-    dataset = "evict",
-    value = round(as.numeric(value),2),
-    stat_type = case_when(
-      str_detect(name,"^median_")    ~ "median",
-      str_detect(name,"rent_burden") ~ "median",
-      str_detect(name,"rate$")       ~ "frac",
-      str_detect(name,"^pct_")       ~ "frac",
-      str_detect(name,"households$|evictions$|filings$") ~ "n",
-      str_detect(name,"flag$|imputed$|subbed$") ~ "flag",
-      TRUE ~ NA_character_
-    ),
-    race = "pooled",
-    gender = "pooled",
-    age_range = "pooled"
-  ) %>%
-  rename(var_name = name) %>%
-  select(
-    dataset,state,county,tract,year,
-    race,gender,age_range,
-    var_name,value,stat_type
-  ) %>%
-  distinct()
 
-# Read each separate state file and append to database
-# This assumes the existence of a system DSN named 'locals', which points to a db 
-locals_db <- DBI::dbConnect(odbc::odbc(), "locals")
-odbc::dbWriteTable(locals_db, "tracts", df, append = T)
+# Push data to the database
 
-rm(df); rm(evict_tract)
+con_tbd_locals <- 
+  DBI::dbConnect(
+    odbc::odbc(),
+    Driver = "SQL Server",
+    Server = Sys.getenv("tbd_server_address"),
+    Database = "locals",
+    UID      = Sys.getenv("tbd_server_uid"),
+    PWD      = Sys.getenv("tbd_server_pw"),
+    Port     = 1433
+  )
+
+
+dbWriteTable(con_tbd_locals
+             ,'tracts'
+            # ,evictions,overwrite = TRUE
+             )
+
+rm(evictions_source,evictions,con_tbd_locals)
